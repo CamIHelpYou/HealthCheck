380c125570b8cd146ff5730b012a2fa75e3d4896
CamIHelpYou <cdl3022@gmail.com>
2019-03-27 21:02:10 -0700

    first commit

---
+++ HealthCheck.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+'''
+@Author: camli
+
+Purpose is to run through a DNA Center health check according to
+https://apps.na.collabserv.com/wikis/home?lang=en-us#!/wiki/W781b350055ce_4000_b067_8140e4587ba4/page/DNAC%20-%20Checklist%20for%20smooth%20upgrade%20experience
+'''
+
+
+import os
+import re
+
+
+class SystemCall(object):
+    '''
+    Object for system calls and their results
+    '''
+    def __init__(self, call):
+        self.call = call
+        self.output = self.command(call)
+        self.problemflag = False
+
+    def command(self, call):
+        return os.popen(call).read()
+
+
+def authentication():
+    '''
+    This function authenticates the user to make all the calls in getoutput()
+    '''
+    os.system("maglev login")
+    print("serial number is: ")
+    os.system("sudo cat /sys/devices/virtual/dmi/id/chassis_serial")
+
+
+def getoutput(Problems):
+    '''
+    This function will be all the command line calls to check
+    '''
+    appliance = SystemCall("cat /sys/devices/virtual/dmi/id/product_name")
+    if "DN1-HW-APL" not in appliance.output:
+        appliance.problemflag = True
+        Problems.append(appliance)
+
+    disk = SystemCall("df -h")
+    usageSearch = re.search(r'(?<=%)\w', disk.output)
+    print(usageSearch)
+
+
+
+'''
+Driver function
+'''
+Problems = []
+authentication()
+getoutput(Problems)
+
+
+
